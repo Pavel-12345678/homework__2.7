0dec3c485673ccee1981afa9e23614da1b6d20f4
Pavel-12345678 <p1980n2014@yandex.ru>
2024-03-27 21:29:13 +0300

    first commit

---
+++ script__2.7.js
@@ -0,0 +1,19 @@
+const answer = 'Мороз';
+answer.toLocaleUpperCase();
+
+function filterStrings(array, StartStr) {
+   return array.filter(item => item.toLowerCase().startWith(StartStr.toLowerCase()));
+}
+
+let num1 = Number(32.58884);
+Math.floor(num1);
+
+let num2 = Number(32.58884);
+Math.ceil(num2); 
+
+let num3 = Number(32.58884);
+Math.round(num3);
+
+let currentDate = new Date();
+console.log(currentDate);
+
